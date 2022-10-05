13c6038d3e440e3f51a9d8129179181ab2f97724
Sergio Beltran <sergiobeltrang91@gmail.com>
2022-10-05 00:20:28 -0300

    Agrupamiento de rutas para mejorar el renderizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import React from "react";
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
 import SignUpPage from "./pages/SignUpPage";
+import Layout from "./components/Layout"
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="about" element={<AboutPage />}/>
-        <Route path="login" element={<LoginPage />}/>
-        <Route path="signup" element={<SignUpPage/>}/>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+
+        <Route path="about" element={<AboutPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<SignUpPage />} />
 
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
